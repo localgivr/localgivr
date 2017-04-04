26038d0dda3544a36896783180309b08d272ead2
Sophia Khan <sophiaxkhan@gmail.com>
2017-04-04 17:36:47 -0400

    Updated headers

--- app-react/src/components/Give.js
+++ app-react/src/components/Give.js
@@ -11,8 +11,6 @@ class Give extends React.Component {
             <h1>Give back to your community.</h1> 
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati fugit molestias doloribus optio ipsam debitis voluptas dolorem nihil, maxime sequi veritatis sunt atque asperiores assumenda reiciendis velit ad iusto tenetur. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet facilis, id quae a in accusantium, molestias amet deserunt. Ipsam, quas cumque, fuga officiis reprehenderit soluta est. Voluptas, unde totam quidem! </p><br /> <br />
 
-            <h1>Give back to your community.</h1> <br /> <br />
-
             <div className="row">
                 <GiveCard />
                 <GiveCard />
