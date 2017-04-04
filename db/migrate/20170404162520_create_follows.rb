e102cb65e20f44d8dab7d5cae5ef7a620c92a52e
Phil <Philip.Shaughnessy@gmail.com>
2017-04-04 14:23:41 -0400

    updated follows migration

--- db/migrate/20170404162520_create_follows.rb
+++ db/migrate/20170404162520_create_follows.rb
@@ -2,7 +2,7 @@ class CreateFollows < ActiveRecord::Migration[5.0]
   def change
     create_table :follows do |t|
       t.references :user, foreign_key: true
-      t.references :followable, foreign_key: true
+      t.references :followable, polymorphic: true, index: true
 
       t.timestamps
     end
