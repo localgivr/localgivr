5a2b9c6639ab39b9614068b90db601523ce88e40
Sophia Khan <sophiaxkhan@gmail.com>
2017-04-05 11:12:03 -0400

    created org profile page base

--- app-react/src/components/OrgProfile.js
+++ app-react/src/components/OrgProfile.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import './css/profile.css'
+import GiveCard from './GiveCard'
 
 class OrgProfile extends React.Component {
     render() {
-        return <div> 
-            <h1>Hello organization profile.</h1>
-        </div>
+        return <div>
+            <div className="row">
+                <div className="col-sm-5">
+                    <ul className="list-unstyled text-center" id="user-profile-box">
+                        <li className="text-uppercase text-center" id="username"><strong><h3>Humane Society of Indianapolis</h3></strong></li><br/>
+                        <li className="text-uppercase text-center" id="location">Indianapolis, IN</li><br/>
+                        <li><button type="button" className="btn btn-success btn-sm edit-profile">Edit Profile</button>
+                        <button type="button" className="btn btn-danger btn-sm delete-profile">Delete Profile</button></li>
+                    </ul>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <div className="user-stats" id="gives">
+                            <h6 className="text-center text-uppercase">Needs Fulfilled</h6> <hr/>
+                            <h1 className="text-center" id="needs-met">##</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6 requests">
+                    <div className="row">
+                        <h2>Open Requests</h2>
+                        <GiveCard />
+                    </div>    
+                    <div className="row">    
+                        <h2>Fulfilled Requests</h2>
+                        <GiveCard /> <GiveCard />
+                    </div>
+                </div>
+            </div>
+        </div>    
     }
 }
 
--- app-react/src/components/UserProfile.js
+++ app-react/src/components/UserProfile.js
@@ -6,10 +6,12 @@ class UserProfile extends React.Component {
         return <div>
             <div className="row">
                 <div className="col-sm-5">
-                    <ul className="list-unstyled" id="user-profile-box">
+                    <ul className="list-unstyled text-center" id="user-profile-box">
                         <li><img className="center-block" id="profile-picture" src="https://unsplash.it/g/200/200?blur" alt="Profile Picture" /></li> <br/>
                         <li className="text-uppercase text-center" id="username"><strong>John Doe</strong></li><br/>
                         <li className="text-uppercase text-center" id="location">Indianapolis, IN</li><br/>
+                        <li><button type="button" className="btn btn-success btn-sm edit-profile">Edit Profile</button>
+                        <button type="button" className="btn btn-danger btn-sm delete-profile">Delete Profile</button></li>
                     </ul>
                     <div className="row">
                         <div className="col-sm-12">
@@ -17,7 +19,6 @@ class UserProfile extends React.Component {
                             <h6 className="text-center text-uppercase">Needs Met</h6> <hr/>
                             <h1 className="text-center" id="needs-met">##</h1>
                             </div>
-                            
                         </div>
                     </div>
                 </div>
