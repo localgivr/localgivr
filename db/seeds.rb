7412db25fb2f1dd78fbdc883136792e7001f0c5e
Phil <Philip.Shaughnessy@gmail.com>
2017-04-05 21:24:41 -0400

    finished initial seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ def new_users
       img_url: Faker::Avatar.image
     )
     users << user
+    user.types << @types.sample
   end
   users
 end
@@ -31,7 +32,7 @@ def new_orgs
   5.times do
     print '.'
     org = Org.create!(
-      name: Faker::Cat.breed,
+      name: Faker::Space.agency,
       password: "password",
       street: Faker::Address.street_address,
       city: Faker::Address.city,
@@ -46,9 +47,41 @@ def new_orgs
   orgs
 end
 
+# def get_cats(cats)
+#   cats.map{ |c| Cat.find_or_create_by(name: c)}
+# end
+#
+# def get_types(types)
+#   types.map{ |t| Type.find_or_create_by(name: t)}
+# end
+
+def new_needs(orgs)
+  orgs.each do |o|
+    need = o.needs.create!(
+      title: Faker::Space.planet,
+      story: Faker::Hipster.paragraph,
+      amount: rand(1..100),
+      expiration: rand(2..30).days.from_now,
+      link: Faker::Internet.url,
+      completed: false,
+      img_url: Faker::LoremPixel.image
+    )
+    need.type = @types.sample
+    Cating.find_or_create_by(need: need, cat: @cats.sample)
+    Cating.find_or_create_by(need: need, cat: @cats.sample)
+    need.save
+  end
+end
+
+@cats = %w(animals culture community education health environment social)
+@types = %w(donation supplies volunteer)
 
 Faker::Config.locale = 'en-US'
 
-userslist = User.all.length < 5 ? new_users : User.all
+@cats.map!{ |c| Cat.find_or_create_by(name: c)}
+@types.map!{ |t| Type.find_or_create_by(name: t)}
 
+userslist = User.all.length < 5 ? new_users : User.all
 orgslist = Org.all.length < 5 ? new_orgs : Org.all
+
+new_needs(orgslist) if Need.all.count < 10
