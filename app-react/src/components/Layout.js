dba56a2b4164d7a12edbd6e1fc39fd8037cb379c
Sophia Khan <sophiaxkhan@gmail.com>
2017-04-06 12:41:05 -0400

    Added signin function

--- app-react/src/components/Layout.js
+++ app-react/src/components/Layout.js
@@ -1,15 +1,50 @@
 import React from 'react';
-// import { browserHistory } from 'react-router'
+//import { browserHistory } from 'react-router'
+import { Form, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 // import React, { Component } from 'react';
 import './css/layout.css'
 
 class Layout extends React.Component {
-  // constructor(props) {
-  //   super(props)
+  constructor(props) {
+    super(props)
+    this.signin=this.signin.bind(this)
+      this.state = {
+        email: '',
+        password: ''
+            }
+  }
 
-  //   this.state ={}
+  signin() {
+    fetch('/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
 
-  // }
+      body: JSON.stringify({
+        user: { 
+          email: this.state.email,
+          password: this.state.password
+        }
+      })
+    })
+
+      .then(function(response) {
+        return response.json();
+          })
+      .then(function(response) {
+        console.log(response);
+      
+        if (response.user.token) {
+          sessionStorage.setItem('user', JSON.stringify(response));
+          location.href = './give';
+                }
+        else {
+            alert('There was an error. Please view your console.');
+            console.log(response);
+        }
+  })
+  }
 
   render() {
     return <div>
@@ -19,12 +54,22 @@ class Layout extends React.Component {
         <h3>local<strong>givr.</strong></h3>
       </div>
         <div className="col-sm-6">
+        <Form inline>
+    <FormGroup controlId="signin">
+      <ControlLabel>Email</ControlLabel>
+      <FormControl type="text" placeholder="Email" />
+    </FormGroup>
+    <FormGroup controlId="signinPass">
+      <ControlLabel>Password</ControlLabel>
+      <FormControl type="password" placeholder="password" />
+    </FormGroup>
+    </Form>
           <ul className="list-inline pull-right">
             <li>
-              <button type="button" className="btn btn-default">Sign Up</button>
+              <button type="button" className="btn btn-default signUp">Sign Up</button>
             </li>
             <li>
-              <button type="button" className="btn btn-default">Log In</button>
+              <button type="button" className="btn btn-default logIn" onClick={this.signin}>Log In</button>
             </li>
           </ul>
         </div>
