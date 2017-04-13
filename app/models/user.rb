76383797514797ed7c7f1ff852b856c75b5895ce
Phil <Philip.Shaughnessy@gmail.com>
2017-04-13 08:48:39 -0400

    finished initial feed method for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,48 +14,11 @@ class User < ApplicationRecord
   validates :email, :phone, uniqueness: true
   validates  :first_name, :last_name, :email, :zip, presence: true
 
-  def feed
-    Need.joins(:cats)
-        .where("cats.id" => self.follows.where(followable_type: "Cat").pluck(:followable_id))
-        .where(org_id: self.follows.where(followable_type: "Org").pluck(:followable_id))
-        .where(type_id: self.typings.pluck(:type_id)).active
-        #.where location check
+  def feed(num = 1)
+    result = Need.joins(:cats)
+    result = result.where("cats.id" => self.follows.where(followable_type: "Cat").pluck(:followable_id)) if self.follows.exists?(followable_type: "Cat")
+    result = result.where(org_id: self.follows.where(followable_type: "Org").pluck(:followable_id)) if self.follows.exists?(followable_type: "Org")
+    result = result.where(type_id: self.typings.pluck(:type_id))
+    result = result.within(20, origin: self).active.limit(num)
   end
 end
-
-
-# Need.joins(:types, :cats).where("types.id IN ?", user.types.ids).where("needs.org_id IN ? OR cats.id IN ?", user.followed_orgs.ids, user.followed_cats.ids)
-#
-# .where("needs.location IN ?", user.zip)
-# .where("needs.location IN ?", user.zip.citystate)
-#
-# # user.types.joins({:needs => :cats}).where("needs.org_id IN ? OR cats.id IN ?", user.followed_orgs.pluck(:id), user.followed_cats.ids)
-
-
-## working:
-#{
-# active scope - expiration
-# Need.where("zip = ?", user.zip)
-# Need.where(type_id: User.last.typings.pluck(:type_id))
-# Need.where(org_id: u.follows.where(followable_type: "Org").pluck(:followable_id))
-# Need.joins(:cats).where("cats.id" => u.follows.where(followable_type: "Cat").pluck(:followable_id))
-#}
-
-
-# trying
-# Need.where(type_id: User.last.types.ids)
-# Need.where(type_id: User.last.typings.pluck(:type_id))
-# Need.where("")
-
-
-# User.last.typings.pluck(:type_id) #works to get type_id
-
-#cats
-# Need.includes(:cats).where("cats.id" => u.follows.where(followable_type: "Cat").pluck(:followable_id))
-
-
-#u.follows.where(followable_type: "Cat").pluck(:followable_id)
-
-# Need.where(org_id: u.follows.where(followable_type: "Org").pluck(:followable_id))
-# Need.includes(:cats).where("cats.id" => u.follows.where(followable_type: "Cat").pluck(:followable_id))
-#Need.joins(:cats).where("cats.id" => u.follows.where(followable_type: "Cat").pluck(:followable_id))
