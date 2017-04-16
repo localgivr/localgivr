b8630764c32739a04b84b89025ec3c68188104f7
Sophia Khan <sophiaxkhan@gmail.com>
2017-04-16 13:42:49 -0400

    Dynamic user info rendering in profile

--- app-react/src/components/ProfileView.js
+++ app-react/src/components/ProfileView.js
@@ -6,11 +6,9 @@ constructor(props) {
         this.getProfile = this.getProfile.bind(this)
 
         this.state = {
-            user: {
-                first_name: '',
-                zip: ''
-            }
+            user: {}
         }
+
     }
 
      getProfile() {
@@ -18,7 +16,7 @@ constructor(props) {
         fetch('api/users/profile?token=' + token)
         .then(res => res.json())
        // .then(res => console.log(res)) 
-        .then(res => this.setState({user: res.user}))
+        .then(res => {this.setState({user: res.user})})
     }
 
     componentDidMount() {
@@ -28,9 +26,9 @@ constructor(props) {
     render() {
         return  <div>
                     <ul className="list-unstyled text-center" id="user-profile-box">
-                        <li><img className="center-block" id="profile-picture" src="https://unsplash.it/g/200/200?blur" alt="Profile Picture" /></li> <br/>
-                        <li className="text-uppercase text-center" id="username"><strong>{this.props.first_name}</strong></li><br/>
-                        <li className="text-uppercase text-center" id="location">{this.props.zip}</li><br/>
+                        <li><img className="center-block" id="profile-picture" src={this.state.user.img_url} alt="Profile Picture" /></li> <br/>
+                        <li className="text-uppercase text-center" id="username"><strong>{this.state.user.first_name}</strong></li><br/>
+                        <li className="text-uppercase text-center" id="location">{this.state.user.zip}</li><br/>
                         {/*<li>
                         <button type="button" className="btn btn-danger btn-sm delete-profile">Delete Profile</button></li>*/}
                     </ul>
