bd353fece64cc78dd58e842612daf82a4c733d8a
Phil <Philip.Shaughnessy@gmail.com>
2017-04-15 21:29:31 -0400

    test stuff

--- app-react/src/components/Homepage.js
+++ app-react/src/components/Homepage.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
 class Homepage extends React.Component {
+
+  constructor(props) {
+      super(props)
+
+      this.getStuff.bind(this)
+
+      this.state = {
+        obj: {}
+      }
+  }
+
+  getStuff() {
+    var token = "FKvqzBN4mDawVZauadSHynVm"
+    fetch('/api/users/profile?token='+token)
+    .then(res => res.json())
+    .then(res => obj: res)
+
+  }
+
+  componentDidMount() {
+      this.getStuff()
+  }
+
     render() {
+        // console.log(obj)
         return <div className="container">
             <h1>Hello homepage.</h1>
+            // <h2>this.state.obj</h2>
         </div>
     }
 }
