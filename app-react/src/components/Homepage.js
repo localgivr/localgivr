19be299842ee80b6e4abe966da56d68781c744b2
Phil <Philip.Shaughnessy@gmail.com>
2017-04-16 18:04:49 -0400

    messed with homepage

--- app-react/src/components/Homepage.js
+++ app-react/src/components/Homepage.js
@@ -5,19 +5,26 @@ class Homepage extends React.Component {
   constructor(props) {
       super(props)
 
-      this.getStuff.bind(this)
-
+      this.getStuff = this.getStuff.bind(this)
+      this.addAttr = this.addAttr.bind(this)
       this.state = {
-        obj: {}
+        user: {},
+        test: "hello"
       }
   }
 
   getStuff() {
     var token = "FKvqzBN4mDawVZauadSHynVm"
-    fetch('/api/users/profile?token='+token)
+    fetch('/api/users/profile?token=' + token)
     .then(res => res.json())
-    .then(res => obj: res)
+    .then(res => {this.setState({user: res.user})})
 
+  }
+
+  addAttr(key, value) {
+    this.setState({
+      [key]: value
+    })
   }
 
   componentDidMount() {
@@ -25,10 +32,13 @@ class Homepage extends React.Component {
   }
 
     render() {
-        // console.log(obj)
+        console.log(this.state)
+        // this.addAttr("test", "pass?") //this is bad - lots of looping errors
+        const {user} = this.state
+
         return <div className="container">
             <h1>Hello homepage.</h1>
-            // <h2>this.state.obj</h2>
+            <h2>{user.first_name} {user.last_name}</h2>
         </div>
     }
 }
