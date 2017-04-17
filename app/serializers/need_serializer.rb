ecedc3bb875e354dd21e74bf6f3c54f4453e8c5f
Phil <Philip.Shaughnessy@gmail.com>
2017-04-17 10:32:57 -0400

    adjusted need serializer to include cats, added photo

--- app/serializers/need_serializer.rb
+++ app/serializers/need_serializer.rb
@@ -2,16 +2,26 @@ class NeedSerializer < ActiveModel::Serializer
   attributes :id, :title, :story, :amount, :expiration, :link, :completed, :img_url, :city, :state
   has_one :org
   has_one :type
+  has_many :cats
 
   def org
+    # object.org.name
     {
       id: object.org.id,
       name: object.org.name,
-      city: object.org.city,
-      state: object.org.state,
-      zip: object.org.zip,
+      # city: object.org.city,
+      # state: object.org.state,
+      # zip: object.org.zip,
       verified: object.org.verified
     }
+  end
+
+  def type
+    object.type.name
+  end
+
+  def cats
+    object.cats.map { |c| c.name }
   end
 
 end
