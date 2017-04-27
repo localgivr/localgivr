e2c37bbd8e77e28a13ab7eba3bbb3992423463f3
Phil <Philip.Shaughnessy@gmail.com>
2017-04-27 10:31:25 -0400

    added demoseed, changed expiration settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) 
+    @user = User.new(user_params)
     if @user.save
       # UsersMailer.signup(@user).deliver
       render json: @user, serializer: UserExtendedSerializer
